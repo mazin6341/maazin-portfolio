597220e2ce0b63c5eaacf282c9514fed9ab29b4f
Mazin6341 <mazin6341@gmail.com>
2026-04-21 14:37:22 +0500

    Add Vite + React + TypeScript + Tailwind v4 project

---
+++ src/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="comingSoon">
+        <div className="flex items-center justify-center min-h-screen">
+          <h1 className="font-mono">Maazin / dev &nbsp;|&nbsp; Coming Soon</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
